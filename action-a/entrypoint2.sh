f6d0a584d6f3e4582cea80550adedc0752661b1f
ArthurHodot <56608499+ArthurHodot@users.noreply.github.com>
2020-08-05 09:15:34 +0200

    Update entrypoint2.sh

--- action-a/entrypoint2.sh
+++ action-a/entrypoint2.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -l
 
-sh -c "echo Hello world my name is $INPUT_MY_NAME, this is an other test"
+echo "echo Hello world my name is $INPUT_MY_NAME, this is an other test" > /github/home/test
